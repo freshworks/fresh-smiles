e8449090341c35390a770049400d9117f9b65d2e
Kai Armstrong <kai.armstrong@gmail.com>
2013-10-02 20:36:02 -0500

    sample config file

---
+++ inc/config-sample.php
@@ -0,0 +1,27 @@
+<?php
+
+// ** This is a SAMPLE file. Add your information and rename the file config.php ** //
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for Smiley */
+define('DB_NAME', 'DATABASE_NAME');
+
+/** MySQL database username */
+define('DB_USER', 'DATABASE_USER');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'DATABASE_PASS');
+
+/** MySQL hostname */
+define('DB_HOST', 'DATABASE_HOST');
+
+
+// ** FreshDesk API Settings ** //
+/* Freshdesk URL */
+define('FD_URL', 'FRESHDESK_URL');
+
+/* Freshdesk API User Email or API Key */
+define('FD_API_USER', 'FRESHDESK_API_KEY');
+
+/* Freshdesk User Password or Blank if using API Key */
+define('FD_API_PASS', '');
