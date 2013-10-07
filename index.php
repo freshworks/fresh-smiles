f6c8f65a12701bcfdac0fc25704a77afa2f09767
Kai Armstrong <kai.armstrong@gmail.com>
2013-10-07 14:56:16 -0500

    smiley layout from Justin

--- index.php
+++ index.php
@@ -1,1 +1,65 @@
-<?php
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en-US" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en-US" > <!--<![endif]-->
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Smiles</title>
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/css/foundation.css">
+<link rel="stylesheet" href="assets/css/layout.css">
+<script src="assets/js/custom.modernizr.js"></script>
+<!--[if lt IE 9]>
+	<script type="text/javascript" src="assets/js/html5shiv.js"></script>
+	<script type="text/javascript" src="assets/js/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+
+<div id="page" class="site">
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<h1 class="site-title"><i class="icon-smile icon-2x happy"></i><br>ZippyKid Customer Support<br>Happiness Report</h1>
+		</div>
+	</header><!-- #masthead -->
+
+	<div class="row">
+		<div class="large-6 columns">
+      <h4>How we measure customer happiness</h4>
+
+      <p>After every interaction with our support team, a customer is asked to rate the experience by clicking one of three ratings: “It was great” (happy face), “It was OK” (flat-line face), or “It wasn’t good” (frown face). <a href="/assets/email.png">Here’s what the choices look like</a> on the ticket.</p>
+
+      <h4>This is how we get better</h4>
+      <p>We study these ratings to help us improve. Every customer interaction is an opportunity to get better. The ultimate goal is 100% happiness.</p>
+
+      <h4>Full transparency and accountability</h4>
+      <p>We’ve made these ratings public so everyone knows how we’re doing. We want to be held to the highest possible standards for customer service. Full transparency keeps us honest.</p>
+		</div>
+		<div class="large-6 columns">
+			<div class="card panel">
+				<h4 class="card-title">Last 100 customer ratings</h4>
+				<div class="overall">
+					<i class="overall-score icon-smile happy"></i> <label class="label happy-score">96% said great!</label>
+				</div>
+				<ul class="small-block-grid-2">
+					<li><i class="icon-2x icon-meh meh"></i> 1 said just OK</li>
+					<li><i class="icon-2x icon-frown unhappy"></i> 3 said not so good</li>
+				</ul>
+				<div class="response">
+					<p>Average wait time for an answer is<br> <span>1 minutes</span> during business hours.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="assets/js/holder.min.js"></script>
+</body>
+</html>
