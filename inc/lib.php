fa50de676010909413e508eb7c37e25473e01fbe
Kai Armstrong <kai.armstrong@gmail.com>
2013-10-07 20:52:47 -0500

    functions and layout updates for smiley

--- inc/lib.php
+++ inc/lib.php
@@ -103,3 +103,60 @@ function upperLimit($fd) {
 	return $max;
 }
 
+//Gravatars for Support
+function gravatarHash($user) {
+	$hash = md5($user);
+	return $hash;
+}
+
+//Last 100 Surveys
+function hundredSmiles() {
+	$query = "SELECT * FROM `zk_smiley` WHERE NOT survey_rating = 'NULL' ORDER BY survey_updated_at DESC LIMIT 100";
+	$ratings = mysql_query($query);
+	if (!$ratings) {
+		die('Invalid query: ' . mysql_error());
+	} else {
+		while( $rating = mysql_fetch_object($ratings) ) {
+			if ( $rating->survey_rating == '1' ) {
+				echo '<i class="icon-3x hundred icon-smile happy"></i>';
+			}
+			else if ( $rating->survey_rating == '2' ) {
+				echo '<i class="icon-3x hundred icon-meh meh"></i>';
+			}
+			else if ( $rating->survey_rating == '3' ) {
+				echo '<i class="icon-3x hundred icon-frown unhappy"></i>';
+			}
+			else {
+				//Do Nothing
+			}
+		}
+	}
+}
+
+//Counts for the Rating Card
+function smileyRatings() {
+	$query = "SELECT count(1) as count, survey_rating FROM (SELECT survey_rating FROM `zk_smiley` WHERE NOT survey_rating = 'NULL' ORDER BY survey_updated_at DESC LIMIT 100) t GROUP BY survey_rating";
+	$ratings = mysql_query($query);
+	if (!$ratings) {
+		die('Invalid query: ' . mysql_error());
+	} else {
+		while( $rating = mysql_fetch_object($ratings) ) {
+			if ( $rating->survey_rating == '1' ) {
+				echo '<div class="overall">';
+				echo '<i class="overall-score icon-smile happy"></i> <label class="label happy-score">' . $rating->count . '% said great!</label>';
+				echo '</div>';
+			}
+			else if ( $rating->survey_rating == '2' ) {
+				echo '<ul class="small-block-grid-2">';
+				echo '<li><i class="icon-2x icon-meh meh"></i> ' . $rating->count . ' said just OK</li>';
+			}
+			else if ( $rating->survey_rating == '3' ) {
+				echo '<li><i class="icon-2x icon-frown unhappy"></i> ' . $rating->count . ' said not so good</li>';
+				echo '</ul>';
+			}
+			else {
+				//Do Nothing
+			}
+		}
+	}
+}
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+// ** Get the Smiley Front End Configuration Information ** //
+require_once(__DIR__ . '/inc/config.php');
+require_once(__DIR__ . '/inc/smiley-config.php');
+require_once(__DIR__ . '/inc/lib.php');
+
+//Connect to MySQL
+mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+@mysql_select_db(DB_NAME) or die("Unable to select database");
+
+?>
+
 <!DOCTYPE html>
 <!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en-US" > <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en-US" > <!--<![endif]-->
@@ -39,17 +51,43 @@
 		<div class="large-6 columns">
 			<div class="card panel">
 				<h4 class="card-title">Last 100 customer ratings</h4>
-				<div class="overall">
-					<i class="overall-score icon-smile happy"></i> <label class="label happy-score">96% said great!</label>
-				</div>
-				<ul class="small-block-grid-2">
-					<li><i class="icon-2x icon-meh meh"></i> 1 said just OK</li>
-					<li><i class="icon-2x icon-frown unhappy"></i> 3 said not so good</li>
-				</ul>
-				<div class="response">
-					<p>Average wait time for an answer is<br> <span>1 minutes</span> during business hours.</p>
-				</div>
+				<?php smileyRatings(); ?>
 			</div>
+		</div>
+	</div>
+	
+	<hr />
+	
+	<div class="row">
+		<div class="large-12 columns">
+			<h2 class="center">The team that makes our customer happy</h2>
+			<p>
+			<?php foreach( $support_emails as $name => $email ) {
+				$hash = gravatarHash($email); ?>
+				<div class="large-3 columns">
+					<img src="http://www.gravatar.com/avatar/<?php echo $hash; ?>?s=75" />
+					<?php echo $name; ?>
+				</div>
+			<?php } ?>
+			</p>
+		</div>
+	</div>
+	
+	<hr />
+	
+	<div class="row">
+		<div class="large-12 columns">
+			<h2 class="center">The last 100 customer support ratings.</h2>
+			<?php hundredSmiles(); ?>
+		</div>
+	</div>
+	
+	<hr />
+	
+	<div class="row">
+		<div class="large-12 columns">
+			<h2 class="center">We strive for happiness — not satisfaction.</h2>
+			<p>The customer service industry is obsessed with “customer satisfaction”. We believe that’s too low a bar. Satisfaction is not a measure of success — it’s just enough to get by. We want our customers to be happy. Happiness is success. Happiness is our goal.</p>
 		</div>
 	</div>
 
