4fd91acd4f408a2f599b44ac633296c91f8258ca
Kai Armstrong <kai.armstrong@gmail.com>
2013-10-08 17:26:58 -0500

    added the sample for users

---
+++ inc/smiley-config-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+// ** Smiley User Settings ** //
+/** The email addresses of support users */
+$support_emails = array(
+	'NAME' 		=> 'NAME@DOMAIN.com',
+	'NAME' 		=> 'NAME@DOMAIN.com',
+	'NAME' 		=> 'NAME@DOMAIN.com',
+	'NAME' 		=> 'NAME@DOMAIN.com'
+);
