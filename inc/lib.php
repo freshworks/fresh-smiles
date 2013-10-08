3fafd873c8594de43c69958890df3a6015ab5c56
Kai Armstrong <kai.armstrong@gmail.com>
2013-10-08 16:30:52 -0500

    new smarter loop

--- inc/lib.php
+++ inc/lib.php
@@ -5,11 +5,12 @@
  **/
  
  
-//This is used to return 300 closed ticket IDs for Checking Survey Data
+//This is used to return closed ticket IDs for Checking Survey Data
 function theTickets($fd, $viewId) {
 
 	$tickets = array();
 
+	//$i can be increased to check more tickets
 	for( $i = 1; $i <= 10; $i++ ) {
 		$json = $fd->getTicketView($viewId, $i);
 		
@@ -23,103 +24,66 @@ function theTickets($fd, $viewId) {
 }
 
 //The main loop used to get survey data 
-function theLoop($fd, $i, $max) {
+function theLoop($fd, $tickets) {
 
-	$json = $fd->getTicketSurvey($i);
-	
-	$response = $fd->getLastHttpResponseText();
-	
-	//Stupid FreshDesk doesn't give you status codes for this...
-	if ( $response == "You have exceeded the limit of requests per hour" ) {
-		$result = "api_limit";
-	}
+	foreach( $tickets as $i ) {
 
-	else if ( isset($json->errors->error) ) {
-		//This is a really stupid hack job... 
-		if ( $i < $max ) {
-			$result = "continue";
-		} else {
+		$json = $fd->getTicketSurvey($i);
+	
+		$response = $fd->getLastHttpResponseText();
+	
+		//Stupid FreshDesk doesn't give you status codes for this...
+		if ( $response == "You have exceeded the limit of requests per hour" ) {
+			$result = "api_limit";
+		}
+
+		else if ( isset($json->errors->error) ) { 
 			$result = "stop";
 		}
-	}
 	
-	else if ( !isset($json[0]) ) {
+		else if ( !isset($json[0]) ) {
 		
-			$result = "INSERT INTO zk_smiley(created_at, updated_at, survey_created_at, survey_updated_at, ticket_id, survey_rating) VALUES(NOW(), NOW(), NULL, NULL, '{$i}', NULL) ON DUPLICATE KEY UPDATE updated_at=NOW()";
+		$result = "INSERT INTO zk_smiley(created_at, updated_at, survey_created_at, survey_updated_at, ticket_id, survey_rating) VALUES(NOW(), NOW(), NULL, NULL, '{$i}', NULL) ON DUPLICATE KEY UPDATE updated_at=NOW()";
 		
-	}
-	
-	else if ( is_array($json) ) {
-		foreach($json[0] as $survey_result) {
-		
-			$raw = array();
-		
-			if ( isset($survey_result->updated_at) ) {
-				$raw['survey_updated_at'] = $survey_result->updated_at;
-			}
-			
-			if ( isset($survey_result->created_at) ) {
-				$raw['survey_created_at'] = $survey_result->created_at;
-			}
-			
-			if ( isset($survey_result->rating) ) {
-				$raw['survey_rating'] = $survey_result->rating;
-			}
-			
-			foreach($raw as $key => $val){
-				$safe[$key] = mysql_real_escape_string($val);
-			}
-			
-			$result = "INSERT INTO zk_smiley(created_at, updated_at, survey_created_at, survey_updated_at, ticket_id, survey_rating) VALUES(NOW(), NOW(), '{$safe['survey_created_at']}', '{$safe['survey_updated_at']}', '{$i}', '{$safe['survey_rating']}') ON DUPLICATE KEY UPDATE updated_at=NOW(), survey_created_at='{$safe['survey_created_at']}', survey_updated_at='{$safe['survey_updated_at']}', survey_rating='{$safe['survey_rating']}'";
-						
-			unset($raw);
-			unset($safe);
-			
 		}
+	
+		else if ( is_array($json) ) {
+			foreach($json[0] as $survey_result) {
 		
+				$raw = array();
+		
+				if ( isset($survey_result->updated_at) ) {
+					$raw['survey_updated_at'] = $survey_result->updated_at;
+				}
+			
+				if ( isset($survey_result->created_at) ) {
+					$raw['survey_created_at'] = $survey_result->created_at;
+				}
+			
+				if ( isset($survey_result->rating) ) {
+					$raw['survey_rating'] = $survey_result->rating;
+				}
+			
+				foreach($raw as $key => $val){
+					$safe[$key] = mysql_real_escape_string($val);
+				}
+			
+				$result = "INSERT INTO zk_smiley(created_at, updated_at, survey_created_at, survey_updated_at, ticket_id, survey_rating) VALUES(NOW(), NOW(), '{$safe['survey_created_at']}', '{$safe['survey_updated_at']}', '{$i}', '{$safe['survey_rating']}') ON DUPLICATE KEY UPDATE updated_at=NOW(), survey_created_at='{$safe['survey_created_at']}', survey_updated_at='{$safe['survey_updated_at']}', survey_rating='{$safe['survey_rating']}'";
+						
+				unset($raw);
+				unset($safe);
+			
+			}
+		}
+	
+		else {
+			$result = "danger";
+			}
+	
+		return $result;
+	
 	}
 	
-	else {
-		$result = "danger";
-	}
-	
-	return $result;
-	
-}
-
-//Returns the Lower Limit for new Cron based on last ticket
-function lowerLimitTicket() {
-	$query = "SELECT ticket_id FROM `zk_smiley` ORDER BY id DESC LIMIT 1";
-	$ticket = mysql_query($query);
-	if (!$ticket) {
-    	die('Invalid query: ' . mysql_error());
-	} else {
-		$ticket = mysql_fetch_object($ticket);
-		$ticket = $ticket->ticket_id;
-		return $ticket;
-	}
-}
-
-//Returns the Lower Limit for new Cron based on last ticket WITH survey
-function lowerLimitSurvey() {
-	$query = "SELECT ticket_id FROM `zk_smiley` WHERE NOT survey_rating = NULL ORDER BY id DESC LIMIT 1"; //This needs logic to actually go 100 surveys back and return that ticket ID
-	$ticket = mysql_query($query);
-	if (!$ticket) {
-    	die('Invalid query: ' . mysql_error());
-	} else if ( isset($ticket) ) {
-		return;
-	} else {
-		$ticket = mysql_fetch_object($ticket);
-		$ticket = $ticket->ticket_id;
-		return $ticket;
-	}
-}
-
-//Returns the Upper Limit for Cron
-function upperLimit($fd) {
-	$tickets = $fd->getAllTickets(1);
-	$max = $tickets[0]->display_id;
-	return $max;
 }
 
 //Gravatars for Support
