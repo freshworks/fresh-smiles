567ad2b2214b450ca2fe1b5591ed076b0fc470af
Graham Christensen <graham@grahamc.com>
2013-10-30 20:56:25 -0500

    Asserts aren't great, but they are better than having to manually interpret results.
    Perhaps you can take this and run with it? :) Or roll back the commit and LOL.

--- tests/ticket_test.php
+++ tests/ticket_test.php
@@ -13,6 +13,13 @@ require_once('../inc/FreshdeskRest.php');
 //Require Smiley Library
 require_once('../inc/lib.php');
 
+assert_options(ASSERT_CALLBACK,
+  function($script, $line, $message) {
+    echo "FAIL: $script:$line\n- $message";
+    exit(1);
+  }
+);
+
 //Connect to MySQL
 mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 @mysql_select_db(DB_NAME) or die("Unable to select database");
@@ -20,21 +27,15 @@ mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 //Create New FreshDesk API Object
 $fd = new FreshdeskRest(FD_URL, FD_API_USER, FD_API_PASS);
 
-if ( $fd->getLastHttpStatus() != 200 ) {
-	echo "Unable to connect to FreshDesk";
-}
-
-//Check for Rate Limiting
-if ( $fd->getLastHttpResponseText() == "You have exceeded the limit of requests per hour" ) {
-	echo "*ERROR* API LIMIT REACHED\n";
-}
+assert($fd->getLastHttpStatus() == 200, "Unable to connect to FreshDesk.")
+assert(strpos($fd->getLastHttpResponseText(), "requests per hour") !== false, "API Limit reached.");
 
 //Returns 300 tickets from Closed View
 $tickets = theTickets($fd, CLOSED_VIEW);
-print_r($tickets);
+assert(count($tickets) == 300, "Three hundred tickets should have been closed.");
 
 //This is a ticket that doesn't exist
-echo theLoop($fd, 40000) . "\n";
+assert(theLoop($fd, 40000) == "stop", "We simply should not have this many tickets.");
 
 //This is a ticket that exists with no Survey
 echo theLoop($fd, 32000) . "\n";
